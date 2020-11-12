e33c968837aa4dc018e753d292f784e173c7e9ef
DurgaRath9 <durga.rath9@gmail.com>
2020-11-12 12:05:09 +0530

    Controller changes

--- src/main/java/com/assignment/exc/repository/StockOperationRepository.java
+++ src/main/java/com/assignment/exc/repository/StockOperationRepository.java
@@ -5,7 +5,6 @@ import com.assignment.exc.model.ShareDetails;
 import com.assignment.exc.utils.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
