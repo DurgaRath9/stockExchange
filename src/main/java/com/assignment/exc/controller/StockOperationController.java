fd96fdfde8a3a3c02fa18ea491f0b8e855052c9f
DurgaRath9 <durga.rath9@gmail.com>
2020-11-12 10:59:35 +0530

    modified App name

--- src/main/java/com/assignment/exc/controller/StockOperationController.java
+++ src/main/java/com/assignment/exc/controller/StockOperationController.java
@@ -3,11 +3,13 @@ package com.assignment.exc.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StockOperationController {
 
     public static final Logger logger = LoggerFactory.getLogger(StockOperationController.class);
+
 
 
 
--- src/main/java/com/assignment/exc/repository/StockOperationRepository.java
+++ src/main/java/com/assignment/exc/repository/StockOperationRepository.java
@@ -3,8 +3,15 @@ package com.assignment.exc.repository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
-@Repository
+/**
+ * This class is used for storing and retrieving data.
+ * Currently we are storing data in Map instead of connecting to DB.
+ */
+@Service
 public class StockOperationRepository {
     public static final Logger logger = LoggerFactory.getLogger(StockOperationRepository.class);
+
+
 }
