07ac05afa7f77539fd0dd98835c3e8589de43ef8
sharai1 <shaiesh.rai@tesco.com>
2020-11-12 12:09:34 +0530

    Added test cases

--- src/test/java/com/assignment/exc/controller/StockOperationControllerTest.java
+++ src/test/java/com/assignment/exc/controller/StockOperationControllerTest.java
@@ -1,8 +1,37 @@
 package com.assignment.exc.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.assignment.exc.model.Share;
+import com.assignment.exc.service.StockOperationService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.ResponseEntity;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-class StockOperationControllerTest {
+@RunWith(MockitoJUnitRunner.class)
+public class StockOperationControllerTest {
 
+    @InjectMocks
+    private StockOperationController stockOperationController;
+    @Mock
+    private StockOperationService stockOperationService;
+
+    @Test
+    public void should_get_all_stock(){
+        List<Share> shares = new ArrayList<>();
+        Share share = new Share();
+        share.setShareId("testId");
+        shares.add(share);
+        Mockito.when(stockOperationService.getAllShares()).thenReturn(shares);
+        ResponseEntity allShares = stockOperationController.getAllShares();
+        assertThat(allShares).isNotNull();
+
+    }
 }
